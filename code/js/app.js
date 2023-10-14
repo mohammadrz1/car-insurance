c314f6e436cf8a66bc68a9bcc8a948c0b2b87b27
mohammadrz1 <127238863+mohammadrz1@users.noreply.github.com>
2023-10-14 14:30:16 +0300

    add sal and error

--- code/js/app.js
+++ code/js/app.js
@@ -1,25 +1,145 @@
-// ---- Calculate the basic price of insurance for each car ----
-function basePriceCalculation(){
-    // document.getElementsByClassName("praid").addEventListener('click', (evt) => {
-    //     console.log("mmdi");
-    // });
+// Variables
+const form = document.querySelector('#request-quote')
 
-   
-    function mmdi(){
-        console.log("mmdi");
-    }
-}
+
+// Events
+document.addEventListener('DOMContentLoaded', afterLoad)
+document.addEventListener('submit', submitForm)
 
 
 // Functions
-// User Interface Functions
+function afterLoad() {
+    displayYears()
+}
+// submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    // read value from the form
+    const make = document.querySelector('#make').value
+    const year = document.querySelector('#year').value
+    const level = document.querySelector('input[name="level"]:checked').value
+
+    // check the value of fileds are correct
+    if (make === "" || year === "" || level === "") {
+        displayMsg('لطفاً مقادیر فرم را با دقت پر نمایید. با تشکر')
+    } else {
+        // STEP1: get info
+        let insuranceCase = {
+            make: make,
+            year: year,
+            level: level
+        }
+
+        // STEP2: calculate
+        calculatePrice(insuranceCase)
+
+        // STEP3: show result message box
+    }
+}
+
+function calculatePrice(info) {
+    let price = 0, base = 2000000
+
+    // + Calculate Make 
+    /* 
+    make:1      =>      1.15
+    make:2      =>      1.30
+    make:3      =>      1.80
+    */
+    const make = info.make
+    switch (make) {
+        case "1":
+            price = base * 1.15
+            break;
+        case "2":
+            price = base * 1.30
+            break;
+        case "3":
+            price = base * 1.80
+            break;
+    }
+
+
+
+    // + Calculate Year
+    // get the year
+    const year = info.year
+    // diffrence = getYearDiffrence(year)
+    const diffrence = function (year) {
+        // Convert to number
+        let
+            persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+            arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+            fixNumbers = function (str) {
+                if (typeof str === 'string') {
+                    for (var i = 0; i < 10; i++) {
+                        str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+                    }
+                }
+                return parseInt(str);
+            };
+
+        // get max year
+        const now = new Date().toLocaleDateString('fa-IR')
+        let nowYear = now.slice(0, 4)
+        let max = fixNumbers(nowYear)
+        year = max - year
+
+        return year
+    }
+    // 3% cheaper for each year
+    price = price - ((diffrence(year) * 3) / 100) * price
+
+    console.log(price);
+
+
+    // + get the level
+    const level = info.level
+    price = calculateLevel(level , price)
+}
+
+function calculateLevel(level , price){
+    /*
+        basic   =>  increase 30%
+        complete=>  increase 50%
+    */
+
+    if (level == 'basic'){
+        // price = price + (price * 0.30) (bara mehrdad)
+        price = price * 1.3
+    }else{
+        price = price * 1.5
+    }
+
+    return price
+}
+
+
+// User Interface (UI) Functions
+// Display message box
+function displayMsg(msg) {
+    // create message box
+    const messageBox = document.createElement('div');
+    messageBox.classList = 'error'
+    messageBox.innerText = msg
+
+    // show message
+    form.insertBefore(messageBox, document.querySelector('.form-group'))
+
+    // remove message box
+    setTimeout(() => {
+        document.querySelector('.error').remove()
+    }, 5000)
+}
+
 // Show Years
 function displayYears() {
     // Convert to number
     let
         persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
         arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
-        fixNumbers = function (str) {
+        fixNumbers = function (str = "") {
             if (typeof str === 'string') {
                 for (var i = 0; i < 10; i++) {
                     str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
@@ -36,14 +156,20 @@ function displayYears() {
 
     // get max year
     let maxYear = fixNumbers(curentYear)
-    console.log(maxYear);
 
     // get min year
     let minYear = maxYear - 20
-    console.log(minYear);
 
     // access to the select tag
     const selectYear = document.querySelector('#year')
+
+    // create first option tag for title
+    // create option tag
+    const optionTag = document.createElement('option')
+    optionTag.innerText = `- انتخاب -`;
+    // optionTag.value = ''
+    // append option to the selectYear
+    selectYear.appendChild(optionTag)
 
     // create for loop for making option tag
     for (let i = maxYear; i >= minYear; i--) {
@@ -58,4 +184,4 @@ function displayYears() {
 }
 
 
-displayYears()
+
